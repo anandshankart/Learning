4614067bfbf4dfb0fa0b441db953c56fdf43e57c
Thangavel, Anand <anathangavel@paypal.com>
2015-01-30 14:16:06 +0530

    no message

--- src/main/java/com/anand/bank/DataStructures/BinaryTree.java
+++ src/main/java/com/anand/bank/DataStructures/BinaryTree.java
@@ -1,9 +1,27 @@
 package com.anand.bank.DataStructures;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class BinaryTree {
 
+	
+	List <BinaryTree> strings;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList a = new ArrayList();
+		a.add("Anand");
+		a.add("Ashok");
+		a.add("BV");
+		a.add("BC");
+		
+		ListIterator s = a.listIterator();
+		while(s.hasNext()){
+			System.out.println(s.next());
+			
+		}
 
 	}
 
